9647118f330021cc04204c6cce48fd0d9d374bec
wenlongzh520-git <wenlongzh520@163.com>
2020-11-12 13:27:29 +0800

    2020-11-12  spark更新

---
+++ src/main/java/com/bigdata/spark/SparkDemo01.java
@@ -0,0 +1,40 @@
+package com.bigdata.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+
+public class SparkDemo01 {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf()
+                .setAppName("SparkDemo01")
+                .setMaster("local");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> lines = sc.textFile("H:\\Spark\\Spark从入门到精通（Scala编程、案例实战、高级特性、Spark内核源码剖析、Hadoop高端）\\课程代码\\spark-study-java\\src\\main\\java\\cn\\spark\\study\\core\\HDFSFile.java");
+
+        JavaRDD<Integer> lineLength = lines.map(new Function<String, Integer>() {
+            @Override
+            public Integer call(String v1) throws Exception {
+                return v1.length();
+            }
+        });
+
+        int count = lineLength.reduce(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+
+        System.out.println("文件总字数是： " + count);
+
+        sc.close();
+    }
+
+
+
+}
